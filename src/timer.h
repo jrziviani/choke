dbca1f0e61b13063f25118f55435bbcc59c8b5b0
Jose Ricardo Ziviani <jose@ziviani.net>
2018-09-08 01:47:13 -0300

    Add simple timer source file
    Signed-off-by: Jose Ricardo Ziviani <jose@ziviani.net>

---
+++ src/timer.h
@@ -0,0 +1,21 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "clib.h"
+
+#include <util/delay.h>
+
+namespace soc
+{
+    inline void delay_us(double usec)
+    {
+        return _delay_us(usec);
+    }
+
+    inline void delay_ms(double usec)
+    {
+        return _delay_ms(usec);
+    }
+}
+
+#endif // TIMER_H
